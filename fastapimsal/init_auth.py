de30fcfa431c5e4c9bc5eb5f64fe417fa1dfc595
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-10-03 14:00:54 +0100

    Redirect state

--- fastapimsal/init_auth.py
+++ fastapimsal/init_auth.py
@@ -67,5 +67,7 @@ def init_auth(
     async def exception_handler(
         request: Request, _: RequiresLoginException
     ) -> Response:
-        "Redirect to homepage if login fails"
-        return RedirectResponse(url=request.url_for(home_name))
+        "Redirect to login with original URL as redirect parameter"
+        login_url = request.url_for("login")
+        current_url = str(request.url)
+        return RedirectResponse(url=f"{login_url}?redirect={current_url}")
